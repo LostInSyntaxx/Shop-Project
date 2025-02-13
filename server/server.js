704ec223f145cda2978bc3a0ca88ba9eb416ad25
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-14 03:19:16 +0700

    UpdateCode

--- server/server.js
+++ server/server.js
@@ -3,14 +3,15 @@ const app = express();
 const morgan = require('morgan');
 const { readdirSync } = require('fs');
 const cors = require('cors');
-const authRoutes = require('./routes/auth');
-const categoryRoutes = require('./routes/category');
+//const authRoutes = require('./routes/auth');
+//const categoryRoutes = require('./routes/category');
 
 
 
 app.use(morgan('dev'));
-//app.use(express.json({limit:'20mb'}));
+app.use(express.json());
 app.use(cors());
+
 
 
 readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
