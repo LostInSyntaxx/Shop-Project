1b86380857f1e61c53b3d942d39b6b9aeec01884
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-13 18:16:42 +0700

    code

---
+++ server/server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const { readdirSync } = require('fs');
+const cors = require('cors');
+//const authRoutes = require('./routes/auth');
+//const categoryRoutes = require('./routes/category');
+
+
+
+app.use(morgan('dev'));
+//app.use(express.json({limit:'20mb'}));
+app.use(cors());
+
+
+readdirSync('./routes').map((r) => app.use('/api', require('./routes/' + r)));
+
+
+
+
+
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
