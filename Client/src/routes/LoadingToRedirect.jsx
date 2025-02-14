1a3f0abc75b4e47c930dd414e00d588a836d98ab
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-15 02:28:15 +0700

    Update LoadingToRedirect.jsx

--- Client/src/routes/LoadingToRedirect.jsx
+++ Client/src/routes/LoadingToRedirect.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React,{ useState, useEffect } from 'react'
+import {Navigate} from "react-router-dom";
 
 const LoadingToRedirect = () => {
+    const [count, setCount] = useState(3)
+    const [ redirect, setRedirect ] = useState(false)
+
+    useEffect(() => {
+        const  interval = setInterval(()=> {
+            setCount((currentCount)=> {
+                if(currentCount===1) {
+                    clearInterval(interval)
+                    setRedirect(true)
+                }
+                return currentCount - 1
+            })
+        },2000)
+
+        return ()=> clearInterval(interval)
+    }, []);
+
+    if(redirect) {
+        return <Navigate to={'/'}/>
+    }
+
     return (
-        <div>LoadingToRedirect</div>
+        <div>No Permission, Redirect in {count}</div>
     )
 }
 export default LoadingToRedirect
