ce9c2e9c89140c1b8ae1006a8fad98bb925343b8
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-15 07:18:31 +0700

    Create HeaderAdmin.jsx

---
+++ Client/src/components/admin/HeaderAdmin.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderAdmin = () => {
+    return (
+        <header className=" bg-white text-black h-16 px-6 flex items-center">
+            header
+        </header>
+    );
+};
+
+export default HeaderAdmin;
