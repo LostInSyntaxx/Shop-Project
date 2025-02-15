66509bec5af1cc0e35a41125c482c83c68431caa
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-16 00:36:30 +0700

    Update HeaderAdmin.jsx

--- Client/src/components/admin/HeaderAdmin.jsx
+++ Client/src/components/admin/HeaderAdmin.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HeaderAdmin = () => {
     return (
-        <header className=" bg-white text-black h-16 px-6 flex items-center">
+        <header className="text-black h-16 px-6 flex items-center">
             header
         </header>
     );
