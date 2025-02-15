bb990b090521f231d2f9b3503cb9c471cbf1372e
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-15 07:18:17 +0700

    Update LayoutAdmin.jsx

--- Client/src/layouts/LayoutAdmin.jsx
+++ Client/src/layouts/LayoutAdmin.jsx
@@ -1,14 +1,24 @@
-import React from 'react'
-import {Outlet} from "react-router-dom";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import SidebarAdmin from '../components/admin/SidebarAdmin.jsx';
+import HeaderAdmin from '../components/admin/HeaderAdmin.jsx';
 
 const LayoutAdmin = () => {
     return (
-        <div>
-            <h1>Sidebar Navbar with dropdown, center logo and icon</h1>
-            <h1>Header</h1>
-            <hr/>
-            <Outlet/>
+        <div className="min-h-screen flex">
+            <div className=" flex">
+                <SidebarAdmin />
+            </div>
+            <div className="flex-1 flex flex-col overflow-hidden">
+                <header className=" shadow-md">
+                    <HeaderAdmin />
+                </header>
+                <main className="flex-1 overflow-x-hidden overflow-y-auto  p-6">
+                    <Outlet />
+                </main>
+            </div>
         </div>
-    )
-}
-export default LayoutAdmin
+    );
+};
+
+export default LayoutAdmin;
