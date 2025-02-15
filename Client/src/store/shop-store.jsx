00087edb9cc0e8b7675503d0a4ffdaa0c957df03
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-16 00:36:35 +0700

    Update shop-store.jsx

--- Client/src/store/shop-store.jsx
+++ Client/src/store/shop-store.jsx
@@ -1,11 +1,14 @@
 import { create } from 'zustand'
 import axios from "axios";
 import { persist,createJSONStorage } from 'zustand/middleware'
+import {listCategory} from "../Api/Main-Api.jsx";
+import {listProduct} from "../Api/Main-api-pro.jsx";
 
 const shopStore = (set)=> ({
     user: null,
     token: null,
-    //categories: [],
+    categories: [],
+    products: [],
     actionLogin: async (form) => {
         const res = await axios.post('http://localhost:3000/api/login',form)
         set({
@@ -14,6 +17,22 @@ const shopStore = (set)=> ({
         })
         return res
     },
+    getCategory : async (token) => {
+        try {
+            const res = await listCategory(token);
+            set({categories: res.data})
+        } catch (err) {
+            console.log(err);
+        }
+    },
+    getProduct : async (token, count) => {
+        try {
+            const res = await listProduct(token,count);
+            set({ products: res.data})
+        } catch (err) {
+            console.log(err);
+        }
+    }
 })
 
 
