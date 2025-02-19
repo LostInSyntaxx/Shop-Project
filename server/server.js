dcba76943469841730a7ba80fc82f0aa26ce743f
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-19 08:01:03 +0700

    Update server.js

--- server/server.js
+++ server/server.js
@@ -3,13 +3,11 @@ const app = express();
 const morgan = require('morgan');
 const { readdirSync } = require('fs');
 const cors = require('cors');
-//const authRoutes = require('./routes/auth');
-//const categoryRoutes = require('./routes/category');
 
 
 
 app.use(morgan('dev'));
-app.use(express.json({limit:'20mb'}));
+app.use(express.json({ limit: '20mb' }));
 app.use(cors());
 
 
