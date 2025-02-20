3417ebf08451d38c2a83657246565fccf1ab56f3
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-20 10:31:30 +0700

    Update Checkout.jsx

--- Client/src/pages/Checkout.jsx
+++ Client/src/pages/Checkout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import CheckoutUser from "../components/card/Checkout-user.jsx";
 
 const Checkout = () => {
     return (
-        <div>Checkout</div>
+        <div>
+            <CheckoutUser/>
+        </div>
     )
 }
 export default Checkout
