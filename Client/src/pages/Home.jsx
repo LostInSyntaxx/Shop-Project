f490946f25a3c66c16e77db8235ac78c95e8be1b
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-23 02:53:50 +0700

    Update Home.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ContentCar from "../components/home/ContentCar.jsx";
+import BestSeller from "../components/home/BestSeller.jsx";
 
 const Home = () => {
     return (
@@ -7,6 +8,7 @@ const Home = () => {
             <ContentCar/>
 
     <p className={'text-2xl text-center mt-4'}>สินค้าขายอย่างดี</p>
+            <BestSeller/>
         </div>
     )
 }
