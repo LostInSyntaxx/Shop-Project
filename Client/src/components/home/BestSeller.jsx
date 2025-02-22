315d82a9c2d32f67afa36f5a190d2bbf67c607b4
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-23 03:11:35 +0700

    Update BestSeller.jsx

--- Client/src/components/home/BestSeller.jsx
+++ Client/src/components/home/BestSeller.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { listProductBy } from "../../Api/Main-api-pro.jsx";
+import ProductCard from "../card/ProductCard.jsx";
 
 const BestSeller = () => {
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        loadData()
+    }, []);
+
+    const loadData = ()=> {
+        listProductBy('sold','desc',10)
+            .then((res)=> {
+                setData(res.data)
+            })
+            .catch((err)=> {
+                console.log(err)
+            })
+    }
+
+
     return (
-        <div>BestSeller</div>
+        <div className={'flex  flex-wrap gap-4 justify-center'}>
+            {
+                data?.map((item,index)=>
+                    <ProductCard item={item}/>
+                )
+            }
+        </div>
     )
 }
 export default BestSeller
