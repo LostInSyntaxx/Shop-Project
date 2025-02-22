9ecf817d3171e71be7d984c5bd80d0ce1cd668e6
LostInSyntaxx <poomabcde11@gmail.com>
2025-02-23 02:52:24 +0700

    Update Home.jsx

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import ContentCar from "../components/home/ContentCar.jsx";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <div>
+            <ContentCar/>
+
+    <p className={'text-2xl text-center mt-4'}>สินค้าขายอย่างดี</p>
+        </div>
     )
 }
 export default Home
